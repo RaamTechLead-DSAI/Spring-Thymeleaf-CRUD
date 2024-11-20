2e954a05bd5636d3e29fa6efbebd5fbd780a10fa
RaamTechLead-DSAI <braamprekash@gmail.com>
2024-11-20 16:08:04 +0000

    Added comments for SubscriptionDAO.java

--- src/main/java/com/RestApiCrud/subscription/dao/SubscriptionDAO.java
+++ src/main/java/com/RestApiCrud/subscription/dao/SubscriptionDAO.java
@@ -4,12 +4,40 @@ import com.RestApiCrud.subscription.entity.Subscription;
 
 import java.util.List;
 
+/**
+ * DAO (Data Access Object) interface for managing Subscription entities.
+ * Provides methods for common CRUD (Create, Read, Update, Delete) operations
+ * on the Subscription table in the database.
+ */
 public interface SubscriptionDAO {
 
+    /**
+     * Retrieves all subscriptions from the database.
+     *
+     * @return a list of all Subscription entities.
+     */
     List<Subscription> findAll();
+
+    /**
+     * Retrieves a specific subscription by its ID.
+     *
+     * @param theId the unique ID of the subscription.
+     * @return the Subscription entity if found; otherwise, null.
+     */
     Subscription findById(int theId);
+
+    /**
+     * Saves a new subscription or updates an existing subscription in the database.
+     *
+     * @param theSubscription the Subscription entity to save or update.
+     * @return the saved or updated Subscription entity.
+     */
     Subscription save(Subscription theSubscription);
+
+    /**
+     * Deletes a subscription from the database by its ID.
+     *
+     * @param theId the unique ID of the subscription to delete.
+     */
     void deleteById(int theId);
-
-
 }
