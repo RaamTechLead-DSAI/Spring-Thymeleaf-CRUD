d217184f10df8c46ecf278d9637f4169a862f0d0
RaamTechLead-DSAI <braamprekash@gmail.com>
2024-11-20 16:16:34 +0000

    Added commentst to main program

--- src/main/java/com/RestApiCrud/subscription/SubscriptionApplication.java
+++ src/main/java/com/RestApiCrud/subscription/SubscriptionApplication.java
@@ -3,9 +3,19 @@ package com.RestApiCrud.subscription;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main entry point for the Spring Boot application.
+ * This class is responsible for bootstrapping and launching the Subscription service.
+ * The @SpringBootApplication annotation indicates this is a Spring Boot application.
+ */
 @SpringBootApplication
 public class SubscriptionApplication {
 
+	/**
+	 * The main method to launch the Spring Boot application.
+	 *
+	 * @param args command-line arguments (unused in this application).
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SubscriptionApplication.class, args);
 	}
